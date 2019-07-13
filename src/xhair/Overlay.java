797ae85f80a526b5b392639e850a8d05068211ad
aeris170 <aeris170@gmail.com>
2019-07-13 07:15:26 +0300

    Implement higher quality rendering of crosshair

--- src/xhair/Overlay.java
+++ src/xhair/Overlay.java
@@ -241,12 +241,12 @@ public final class Overlay extends JWindow {
 		}
 
 		/**
-		 * Process �mage.
+		 * Process image.
 		 *
 		 * @param i the i
 		 * @param fill the fill
 		 * @param surround the surround
-		 * @return the buffered �mage
+		 * @return the buffered image
 		 */
 		public BufferedImage processImage(final BufferedImage i, final Color fill, final Color surround) {
 			if (i == null || fill == null || surround == null) {
@@ -290,7 +290,12 @@ public final class Overlay extends JWindow {
 		@Override
 		public void paintComponent(final Graphics g) {
 			final Graphics2D g2d = (Graphics2D) g;
-			g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+			g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+			g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+			g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+			g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 			if (show) {
 				final int crosshaitStartDrawX = (getWidth() - getCrosshairWidth()) / 2;
 				final int crosshaitStartDrawY = (getHeight() - getCrosshairHeight()) / 2;
